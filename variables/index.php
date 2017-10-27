02624b180e044bed1abdd7910ff652b89f612f37
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-27 00:09:14 -0500

    Tipo de variables

---
+++ variables/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// int 
+$intVar = 1;
+var_dump($intVar);
+
+//float
+$floatVar = 2.2;
+var_dump($floatVar);
+
+
+//string
+$stringVar = 'texto ejemplo';
+var_dump($stringVar);
+
+// bool 
+$boolVar = true;
+var_dump($boolVar);
+
+// null
+$nullVar = null;
+var_dump($nullVar);
+
+// array
+$arrayVar = ['valor1','valor2','valor3',1,2.2];
+var_dump($arrayVar);
