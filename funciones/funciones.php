274f5b143f18ea27332c24365849e55b31f516a7
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 17:35:13 -0600

    scope

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -8,9 +8,14 @@
 hello('Juan');
 hello('Alejo');*/
 
+$x = 100; // $x existe en el scope global pero no dentro de la funciones
+
 function sum($a , $b){
-	return $a + $b;
+	$x = $a + $b;
+	return $x;  // retorna la suma y no el 100 porque esta en un scope diferente
 }
 
 $c = sum(1, 2);
 var_dump($c);
+
+var_dump($x);
