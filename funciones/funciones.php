db4c453bae2b10deab584cea4755880ac2e73053
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 17:24:19 -0600

    Return en funciones

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -1,9 +1,16 @@
 <?php
 
-function hello($name){
+/*function hello($name){
 	echo 'Hello '. $name;
 	echo '<br>';
 }
 
 hello('Juan');
-hello('Alejo');
+hello('Alejo');*/
+
+function sum($a , $b){
+	return $a + $b;
+}
+
+$c = sum(1, 2);
+var_dump($c);
