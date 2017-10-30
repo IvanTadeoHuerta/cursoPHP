668721eafdec8499ab48251578eede505eb96e08
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 17:42:07 -0600

    Estructura condicional

--- condicionales/condicionales.php
+++ condicionales/condicionales.php
@@ -1,7 +1,11 @@
 <?php
 
-$color = 'black';
+$color = 'green2';
 
 if($color == 'black'){
 	echo 'Color es Black';
+}else if($color == 'green') {
+	echo 'El color no es verde';
+}else{
+	echo 'color desconocido';
 }
