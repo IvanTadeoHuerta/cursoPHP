3192438997ccd1f8c6155822f5286d3e27294648
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 09:55:59 -0600

    Incremento

---
+++ operadores/incremento.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 1;
+$a++;
+
+var_dump($a);
+
+//Nota
+$a = 1;
+var_dump($a++); //Imprime uno
+var_dump($a); //Imprime el valor incrementado $a es igual a 2
+
+//Para solucionar lo de arriba ejecutar el post incremento ++$a
