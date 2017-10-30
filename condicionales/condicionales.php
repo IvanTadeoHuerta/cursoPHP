c549a95be39a2031ee783c00a394147c20fb14e9
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 17:44:40 -0600

    Estructura condicional switch

--- condicionales/condicionales.php
+++ condicionales/condicionales.php
@@ -7,5 +7,19 @@ if($color == 'black'){
 }else if($color == 'green') {
 	echo 'El color no es verde';
 }else{
-	echo 'color desconocido';
+	echo 'color desconocido en if';
+}
+
+echo '<br>';
+
+switch ($color) {
+	case 'green':
+		echo 'es verde switch';
+		break;
+	case 'black':
+		echo 'es negro switch';
+		break;
+	default:
+		echo 'Color desconocido en swith';
+		break;
 }
