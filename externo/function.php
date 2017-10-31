a828260f7701a1649266e5aa86d38290ea4167e5
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-31 09:20:17 -0600

    Importando una función

--- externo/function.php
+++ externo/function.php
@@ -1,2 +1,7 @@
 <?php
 echo '<p>Texto desde function.php</p>';
+
+function suma($a, $b){
+	$result = $a + $b ;
+	echo '<p>Resultado: '. $result .'</p>';
+}
--- externo/index.php
+++ externo/index.php
@@ -1,3 +1,5 @@
 <?php
 require 'function.php'; //Detiene la ejecucion y PHP mostrara el error
 echo '<p>Texto desde index.php</p>';
+
+suma(10,20);
