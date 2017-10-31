e159aac9a3926483d5c530d668c89d9456b6e987
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-31 11:38:00 -0600

    Array Map

--- closures/index.php
+++ closures/index.php
@@ -8,3 +8,15 @@ $var = function () use($var2){
 };
 
 $var();
+
+echo '<br>';
+
+$numeros = [1,2,3,4,5];
+$var3 = 3;
+
+$result = array_map(function($n) use($var3){
+	return $n * $var3;
+}, $numeros);
+
+
+var_dump($result);
