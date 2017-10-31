5063219577e97d8f75ed8779f27d6a9acbfd7816
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-31 11:29:24 -0600

    Solved scope local function

--- closures/index.php
+++ closures/index.php
@@ -2,7 +2,7 @@
 
 $var2 = 1;
 
-$var = function (){
+$var = function () use($var2){
 	echo 'Esto es una funcion anonima';
 	echo 'Value: ' . $var2; //Error porque no esta definida dentro del scope local 
 };
