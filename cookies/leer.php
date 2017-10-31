ccf430f2600041e68340a8b54680aed92c181e82
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-31 09:53:25 -0600

    Leer cookie

--- cookies/leer.php
+++ cookies/leer.php
@@ -1,10 +1,4 @@
 <?php
-/* setcookie recibe 3 parametros:
-   1.- Nombre de la cookie
-   2.- Cadena
-   3.- Tiempo de vida en segundos */
 
-
-setcookie('count','1', time() +  60);
-
-echo '<p>Estamos trabajando con cookies</p>';
+//$_COOKIE es una variable super global
+echo 'Cookie valor: ' . $_COOKIE['count']; 
