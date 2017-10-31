2212f858ba155ff87573f612cad0d0aa2cd422c4
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-30 18:00:31 -0600

    Ciclo foreach and do while

--- ciclos/ciclos.php
+++ ciclos/ciclos.php
@@ -11,3 +11,29 @@ while ($i <= 10) {
 	echo $i. '<br>';
 	$i++;
 }
+
+echo '<br>';
+$i=0;
+do{
+	echo $i.'<br>';
+	$i++;
+}while ($i<=10);
+
+echo '<br>';
+
+$arrayPersonas = [
+	0 => 'Ivan',
+	1 => 'Tadeo',
+	2 => 'Huerta',
+	3 => 'Ejemplo'
+];
+
+foreach ($arrayPersonas as $persona) {
+	 echo 'Nombre de la persona: '.$persona . '<br>';
+}
+
+echo '<br>';
+
+foreach ($arrayPersonas as $key => $persona) {
+	echo 'Llave: ' . $key . ' Persona: '.$persona . '<br>';
+}
