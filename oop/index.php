a8c31000e95f91fc9eb911ea243f3c9d0dc70d6d
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-01 12:19:06 -0600

    Importando archivos Car y Truck en index.php

--- oop/index.php
+++ oop/index.php
@@ -1,55 +1,7 @@
 <?php
 
-class Vehicle
-{
-	private $owner = 'Ivan Tadeo Huerta';
-
-	public function __construct($owner){
-		$this->owner = $owner;
-	}
-
-	/*
-	 * __destruct se utiliza generalmente para liberar un recurso o limpiar algo
-	 * en el script
-	 *
-	 */
-	public function __destruct(){
-		echo 'destructor<br>';
-	}
-
-	public function move(){
-		echo 'moving <br>';
-	}
-
-	public function getOwner(){
-		return $this->owner;
-	}
-
-	public function setOwner($owner){
-		$this->owner = $owner;
-	}
-
-}
-
-class Car extends Vehicle{
-	public function move(){
-		echo 'Car: moving <br>';
-	}
-}
-
-class Truck extends Vehicle{
-	private $type;
-
-	public function __construct($owner, $type){
-		$this->type = $type;
-		parent::__construct($owner);
-	}
-
-	public function move(){
-		echo 'Truck '. $this->type .' moving <br>';
-	}
-
-}
+include 'vehicles/Car.php';
+include 'vehicles/Truck.php';
 
 echo '<br>';
 
