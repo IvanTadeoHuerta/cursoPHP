40b4af20ec696767d793309a30e9f05e80d74a35
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-14 10:28:02 -0600

    Limpiando codigo de conexion

--- databases/index.php
+++ databases/index.php
@@ -1,6 +1,10 @@
 <?php 
+$dbHost = 'localhost';
+$dbname = 'cursophp';
+$dbUser = 'root';
+$dbPass = '';
 try{
-	$pdo = new PDO("mysql:host=localhost2;dbname=cursophp","root","");
+	$pdo = new PDO("mysql:host=$dbHost;dbname=$dbname","$dbUser","$dbPass");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e){
 	echo $e->getMessage();
