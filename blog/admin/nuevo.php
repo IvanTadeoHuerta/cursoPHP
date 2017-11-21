b49a9823c88af521f63f5ec995fcd39e45c1c747
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-21 16:26:20 -0600

    Formulario para agregar nuevo post

--- blog/admin/nuevo.php
+++ blog/admin/nuevo.php
@@ -16,43 +16,27 @@ $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h1>Administrador</h1>
+					<h1>Administrador- agregar nuevo post</h1>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-8">
-					<table class="table">
-					  <thead>
-					  	<tr>
-					  		<th>Titulo</th>
-					  		<th>Editar</th>
-					  		<th>Eliminar</th>
-					  	</tr>
-					  </thead>
-					  <tbody>
-							<?php
-							 foreach ($blogPosts as $key => $blog) {
-							 	 echo '<tr>';
-								 echo '<td>' . $blog['titulo'] . '</td>';
-								 echo '<td><button class="btn btn-primary">Editar</button></td>';
-								 echo '<td><button class="btn btn-danger">Eliminar</button></td>';
-								 echo '</tr>';
-								 
-							 }
-							?>
-					  </tbody>
-					</table>	
+					 <form action="/action_page.php" method="post">
+						    <div class="form-group">
+						      <label for="titulo">Email:</label>
+						      <input type="titulo" class="form-control" id="titulo" placeholder="Ingresa titulo" name="titulo">
+  								<label for="contenido">Contenido:</label>
+						      <textarea name="contenido" class="form-control" id="contenido" cols="30" rows="10"></textarea>
+						    </div>
+						    
+						    <button type="submit" class="btn btn-default">ENVIAR</button>
+						  </form>
 				</div>
 				<div class="clo-md-4">
-					<a class="btn btn-success" href="nuevo.php">Agregar nuevo</a>
+					
 				</div>
 			</div>
 			<div class="row">
-			<div class="col-md-12">
-				<footer>
-					Administracion de los post
-				</footer>
-			</div>
 			</div>
 		</div>
 	</body>
