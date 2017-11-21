27e0385615edf3ad1eddd90b8c3d81045cd76615
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-21 15:31:42 -0600

    Se creo plantilla del proyecto

---
+++ blog/index.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>
+			Blog
+		</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Blog Ivan Tadeo</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="blog-post">
+						<h2>Sample tittle</h2>
+						<p>Jan  1, 2020 by <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="https://thumb7.shutterstock.com/display_pic_with_logo/114085/324472871/stock-vector-november-sign-with-colour-confetti-vector-paper-illustration-324472871.jpg" alt ="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+						</div>
+					</div>
+					<div class="blog-post">
+						<h2>Sample tittle</h2>
+						<p>Jan  1, 2020 by <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="https://thumb7.shutterstock.com/display_pic_with_logo/114085/324472871/stock-vector-november-sign-with-colour-confetti-vector-paper-illustration-324472871.jpg" alt ="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+						</div>
+					</div>
+					<div class="blog-post">
+						<h2>Sample tittle</h2>
+						<p>Jan  1, 2020 by <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="https://thumb7.shutterstock.com/display_pic_with_logo/114085/324472871/stock-vector-november-sign-with-colour-confetti-vector-paper-illustration-324472871.jpg" alt ="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+						</div>
+					</div>
+				</div>
+				<div class="clo-md-4">
+					SideBar
+				</div>
+			</div>
+			<div class="row">
+				<footer>
+					Este es un pie de pagina
+				</footer>
+			</div>
+		</div>
+	</body>
+</html>
