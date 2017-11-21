2d7493b19f1880e0a9fa74da92399ddc5ddf152a
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-21 17:42:32 -0600

    Instalando routing con composer

--- blog/vendor/composer/autoload_static.php
+++ blog/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit931cc602f4fc96c811a3ce7bba9ce559
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit931cc602f4fc96c811a3ce7bba9ce559::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit931cc602f4fc96c811a3ce7bba9ce559::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
