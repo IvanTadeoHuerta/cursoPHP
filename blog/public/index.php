39e6ed0e61061d87374934301b60e778e1329c27
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-21 17:47:54 -0600

    Definiendo rutas

--- blog/public/index.php
+++ blog/public/index.php
@@ -2,22 +2,22 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
 include_once '../config.php';
 
 $route = $_GET['route'] ?? '/';
 
-switch ($route) {
-	case '/':
-		require '../index.php';
-		break;
-	case '/admin':
-		require '../admin/index.php';
-	break;
-	case '/admin/posts':
-		require '../admin/posts.php';
-	break;
-	default:
-		break;
-}
+/*
+ *https://packagist.org/packages/phroute/phroute
+ */
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+
+$router->get('/', function (){
+	return 'Route /';
+}); 
 
 ?>
